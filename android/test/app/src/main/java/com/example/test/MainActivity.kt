4b78aea39aeb1e87abff207d201db256d3352692
Ricardo <ricardoengtech@gmail.com>
2025-06-04 20:45:04 -0400

    Lista de Produtos

---
+++ android/test/app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.test.models.Pessoa
+import com.example.test.ui.theme.TestTheme
+import com.example.test.views.FichaPessoal
+import com.example.test.views.ListaDeProdutos
+import com.example.test.views.PessoaCard
+import com.example.test.models.Produto
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    PessoaCard(Pessoa("Ayrton", 30))
+   val produtos = listOf(
+        Produto("Arroz", 8.0, true),
+        Produto("Feijão", 7.5, true),
+        Produto("Macarrão", 4.0, false),
+        Produto("Açúcar", 3.8, true),
+        Produto("Óleo de soja", 9.5, false),
+        Produto("Farinha de trigo", 5.2, true),
+        Produto("Leite", 6.0, true),
+        Produto("Café", 12.0, false),
+        Produto("Manteiga", 14.5, true),
+        Produto("Queijo", 18.0, false),
+        Produto("Presunto", 16.7, true)
+   )
+
+    ListaDeProdutos(produtos)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestTheme {
+        Greeting("Android")
+    }
+}
+
---
+++ android/test/app/src/main/java/com/example/test/views/ListaDeProdutos.kt
@@ -0,0 +1,34 @@
+package com.example.test.views
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.test.models.Produto
+
+@Composable
+fun ListaDeProdutos (produtos: List<Produto>) {
+    Column (modifier = Modifier.padding(32.dp)){
+        Text("Produtos em Promoção!", style = MaterialTheme.typography.titleMedium, fontWeight = FontWeight.Bold)
+
+        produtos.filter { it.emPromocao }.forEach {
+            Text(" - ${it.nome} por R$%.2f".format(it.preco))
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+            val total = produtos.filter { it.emPromocao }.sumOf{ it.preco }
+            val media = produtos.filter { it.emPromocao }.map{ it.preco }.average()
+            val maisCaro = produtos.filter {it.emPromocao}.maxByOrNull { it.preco }
+
+            Text ("Valor total: R$%.2f".format(total), fontWeight = FontWeight.Bold)
+            Text ("Média: R$%.2f".format(media))
+            Text ("Mais caro: ${maisCaro?.nome}, ${maisCaro?.preco}")
+    }
+}
